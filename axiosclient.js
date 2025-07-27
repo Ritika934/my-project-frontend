8d80e1a65151fb8faf467c63e76f90a152a2dcf2
Ritika Parmar <parmarritika9878@gmail.com>
2025-07-28 01:00:26 +0530

    Update axiosclient.js

--- axiosclient.js
+++ axiosclient.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosclient = axios.create({
 
-    baseURL:'https://leetcode-project-pearl.vercel.app',
+    baseURL:'https://my-project-frontend-i27dt9nha-ritika-parmars-projects.vercel.app',
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
